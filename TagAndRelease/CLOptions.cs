c7309ac9467d67c1a33088980c4b7bccb34fd274
Jason Davis <jasondavis303@gmail.com>
2021-04-14 00:53:47 -0600

    Add project files.

---
+++ TagAndRelease/CLOptions.cs
@@ -0,0 +1,32 @@
+﻿using CommandLine;
+using System.Collections.Generic;
+
+namespace TagAndRelease
+{
+    class CLOptions
+    {
+        [Option("owner", Required = true)]
+        public string Owner { get; set; }
+
+        [Option("repo-name", Required = true)]
+        public string RepoName { get; set; }
+
+        [Option("branch", Default ="master")]
+        public string Branch { get; set; }
+
+        [Option("github-token", Required = true)]
+        public string GithubToken { get; set; }
+
+        [Option("set-version", HelpText = "Set the tag/release version. If not specified, it's derived from DateTime.UtcNow")]
+        public string Version { get; set; }
+
+        [Option("assets", HelpText = "Assets to add to the release")]
+        public IEnumerable<string> Assets { get; set; }
+
+        [Option("overwrite")]
+        public bool Overwrite { get; set; }
+
+        [Option("verbose")]
+        public bool Verbose { get; set; }
+    }
+}
---
+++ TagAndRelease/Program.cs
@@ -0,0 +1,89 @@
+﻿using CommandLine;
+using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace TagAndRelease
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Tag and Release");
+            Console.WriteLine($"v{Assembly.GetExecutingAssembly().GetName().Version}");
+
+            int ret = -1;
+
+            try
+            {
+                Parser.Default.ParseArguments<CLOptions>(args).WithParsed(opts => RunAsync(opts).Wait());
+            }
+            catch (AggregateException ex)
+            {
+                ShowExceptions(ex.InnerExceptions);
+            }
+            catch (Exception ex)
+            {
+                ShowExceptions(new Exception[] { ex });
+            }
+
+            return ret;
+        }
+
+        static async Task RunAsync(CLOptions opts)
+        {
+            if (string.IsNullOrWhiteSpace(opts.Version))
+            {
+                opts.Version = TimestampVersion.Generator.Generate().ToString();
+                if (opts.Verbose)
+                    Console.WriteLine("Generated Version: {0}", opts.Version);
+            }
+
+            if (!opts.Version.StartsWith("v", StringComparison.CurrentCultureIgnoreCase))
+                opts.Version = "v" + opts.Version;
+
+            var client = new GitHubClient(new ProductHeaderValue("jasondavis303.TagAndRelease"));
+            var tokenAuth = new Credentials(opts.GithubToken);
+            client.Credentials = tokenAuth;
+
+            bool tagExists = false;
+            try
+            {
+                if (opts.Verbose)
+                    Console.WriteLine("Checking for tag: {0}", opts.Version);
+                var existingTag = await client.Git.Tag.Get(opts.Owner, opts.RepoName, opts.Version);
+                tagExists = existingTag != null;
+            }
+            catch { }
+
+            if (!tagExists)
+            {
+                if (opts.Verbose)
+                    Console.WriteLine("Creating tag: {0}", opts.Version);
+                var commit = await client.Repository.Commit.Get(opts.Owner, opts.RepoName, opts.Branch);
+                var newTag = new NewTag
+                {
+                    Object = commit.Sha.Substring(0, 7),
+                    Tag = opts.Version,
+                    Type = TaggedType.Commit,
+                };
+                await client.Git.Tag.Create(opts.Owner, opts.RepoName, newTag);
+            }
+
+            
+        }
+
+        static void ShowExceptions(IEnumerable<Exception> exes)
+        {
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Red;
+            foreach (var ex in exes)
+                Console.WriteLine(ex.Message);
+            Console.ResetColor();
+            Console.WriteLine();
+        }
+    }
+}
